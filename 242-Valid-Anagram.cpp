82f4efc3c8d3550846a3b05d67f93bd82a89228c
Arman Singh <141248135+arman9919@users.noreply.github.com>
2025-01-10 18:33:10 +0530

    [242] [Time Beats: 58.83%] [Memory Beats: 29.26%] - LeetPush

--- 242-Valid-Anagram.cpp
+++ 242-Valid-Anagram.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(), s.end());
-       sort(t.begin(), t.end());
-       if(t == s) return true;
-       else return false; 
+    // METHOD-1 : using STRING
+    //    sort(s.begin(), s.end());
+    //    sort(t.begin(), t.end());
+    //    if(t == s) return true;
+    //    else return false; 
+
+    // METHOD-2 : using 2 MAPS
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+        for(int i=0 ; i<s.length() ; i++){
+            map1[s[i]]++;
+        }
+        for(int i=0 ; i<t.length() ; i++){
+            map2[t[i]]++;
+        }
+        for(auto x : map1){
+            char ch1 = x.first;
+            int freq1 = x.second;
+            if(map2.find(ch1)!=map2.end()){
+                int freq2 = map2[ch1];
+                if(freq1 != freq2) return false;
+            }
+            else return false;
+        }
+        return true;
+
+
     }
 };
