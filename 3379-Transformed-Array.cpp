20d2464e52a1a4e5758f65e336d2d4661926ad56
Arman Singh <141248135+arman9919@users.noreply.github.com>
2025-01-16 10:55:05 +0530

    [3379] [Time Beats: 20.87%] [Memory Beats: 14.38%] - LeetPush

--- 3379-Transformed-Array.cpp
+++ 3379-Transformed-Array.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         vector<int> result(n, 0);
         for(int i=0 ; i<n ; i++){
-            if(nums[i]>0) result[i] = nums[(i+nums[i]) % n];
-            else if(nums[i]<0) result[i] = nums[(i + nums[i] % n + n) % n];
-            else result[i] = nums[i];
-
-
+            // if(nums[i]>0) result[i] = nums[(i+nums[i]) % n];   // for positive 
+            // else if(nums[i]<0) result[i] = nums[(i + nums[i] % n + n) % n]; //for negative
+            // else result[i] = nums[i];   // for 0
+            result[i] = nums[(i + nums[i] % n + n) % n];
+            
         }
         return result;
         
