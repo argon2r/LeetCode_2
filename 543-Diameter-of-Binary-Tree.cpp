5d1dc4964cb096dc910c32510f0cd5fb91c7ab83
Arman Singh <141248135+arman9919@users.noreply.github.com>
2025-01-24 08:16:10 +0530

    [543] [Time Beats: 5.00%] [Memory Beats: 69.77%] - LeetPush

--- 543-Diameter-of-Binary-Tree.cpp
+++ 543-Diameter-of-Binary-Tree.cpp
@@ -1,28 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int maxDia = 0;
+    // int maxDia = 0;
+    // int levels(TreeNode* root){
+    //     if(root==NULL) return 0;
+    //     return 1 + max(levels(root->left) , levels(root->right));
+        
+    // }
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //     if(root == NULL) return 0;
+    //     int Dia = levels(root->left) + levels(root->right);
+    //     maxDia = max(maxDia , Dia);
+    //     diameterOfBinaryTree(root->left);
+    //     diameterOfBinaryTree(root->right);
+    //     return maxDia;
+    // }
+
+// Interviwer doesn't like using global variable in the code
+// so this code was used 
+// helper fn and maxDia as reference are used to remove the use of global varriable
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1 + max(levels(root->left) , levels(root->right));
-        
     }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL) return 0;
+
+    void helper(TreeNode* root , int &maxDia){
+        if(root == NULL) return;
         int Dia = levels(root->left) + levels(root->right);
         maxDia = max(maxDia , Dia);
-        diameterOfBinaryTree(root->left);
-        diameterOfBinaryTree(root->right);
+        helper(root->left  , maxDia);
+        helper(root->right , maxDia);    
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxDia = 0;
+        helper(root , maxDia);
         return maxDia;
     }
 };
