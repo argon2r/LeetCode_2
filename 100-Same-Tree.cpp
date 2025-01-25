4fa26156ca76ed6d22975b7c92d272f9f0827ec1
Arman Singh <141248135+arman9919@users.noreply.github.com>
2025-01-25 09:46:39 +0530

    [100] [Time Beats: 100.00%] [Memory Beats: 46.01%] - LeetPush

---
+++ 100-Same-Tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL) return true;
+        
+        if(p==NULL && q!=NULL) return false;
+        if(p!=NULL && q==NULL) return false;
+        // the above 2lines can also be replaced by (below)
+        //if(p==NULL || q==NULL) return false;
+        
+        if (p->val != q->val) return false;
+
+        bool rightAns = isSameTree(p->right , q->right);
+        if(rightAns == false) return false;
+
+        bool leftAns = isSameTree(p->left , q->left); 
+        if(leftAns == false) return false;
+
+        return true;  
+    }
+
+    
+};
