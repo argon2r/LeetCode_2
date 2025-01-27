0964cfa3960347645f7e95c4805ce0966fae4ec1
Arman Singh <141248135+argon2r@users.noreply.github.com>
2025-01-27 10:42:42 +0530

    [94] [Time Beats: 100.00%] [Memory Beats: 36.00%] - LeetPush

--- 94-Binary-Tree-Inorder-Traversal.cpp
+++ 94-Binary-Tree-Inorder-Traversal.cpp
@@ -1,18 +1,40 @@
+//-------------------Method-1 : RECURSIVE------------------------
+// class Solution {
+// public:
+//     void inOrder(TreeNode* root , vector<int> &ans){
+//         if(root == NULL) return;
+//         inOrder(root->left , ans);
+//         ans.push_back(root->val);
+//         inOrder(root->right , ans);
+//     }
+
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         inOrder(root , ans);
+//         return ans;      
+//     }
+// };
+
+//-----------Metod-2 : ITERATIVE------------------
 class Solution {
 public:
-
-    void inOrder(TreeNode* root , vector<int> &ans){
-        if(root == NULL) return;
-        inOrder(root->left , ans);
-        ans.push_back(root->val);
-        inOrder(root->right , ans);
-
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inOrder(root , ans);
-        return ans;      
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        
+        while(st.size()>0 || node){
+            if(node){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                TreeNode* temp = st.top();
+                st.pop();
+                ans.push_back(temp->val);
+                node = temp->right;
+            }
+        }
+        return ans; 
     }
 };
-
-
