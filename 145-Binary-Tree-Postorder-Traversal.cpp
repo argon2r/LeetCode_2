8be61b644f3545172e8e0ff8501bbe23d950d509
Arman Singh <141248135+argon2r@users.noreply.github.com>
2025-01-27 10:19:40 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 72.30%] - LeetPush

--- 145-Binary-Tree-Postorder-Traversal.cpp
+++ 145-Binary-Tree-Postorder-Traversal.cpp
@@ -1,18 +1,37 @@
+// Method-1 : RECURSIVE
+// class Solution {
+// public:
+
+//     void postOrder(TreeNode* root , vector<int> &ans){
+//         if(root == NULL) return;
+//         postOrder(root->left , ans);
+//         postOrder(root->right , ans);
+//         ans.push_back(root->val);
+
+//     }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         postOrder(root , ans);
+//         return ans;      
+//     }
+// };
+
+// Method -2  Iterative 
 class Solution {
 public:
-
-    void postOrder(TreeNode* root , vector<int> &ans){
-        if(root == NULL) return;
-        postOrder(root->left , ans);
-        postOrder(root->right , ans);
-        ans.push_back(root->val);
-
-    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        postOrder(root , ans);
-        return ans;      
+        stack<TreeNode*> st;
+        if(root!=NULL)st.push(root);
+        while( st.size()>0 ){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->left!=NULL) st.push(temp->left);
+            if(temp->right!=NULL) st.push(temp->right);
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;         
     }
 };
-
 
