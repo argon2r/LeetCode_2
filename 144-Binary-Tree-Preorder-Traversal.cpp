0e9f24182bb1604932cd8dea508967a71e6d028b
Arman Singh <141248135+argon2r@users.noreply.github.com>
2025-01-27 08:50:22 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 73.17%] - LeetPush

--- 144-Binary-Tree-Preorder-Traversal.cpp
+++ 144-Binary-Tree-Preorder-Traversal.cpp
@@ -1,16 +1,36 @@
+// Method-1 : PreOrder RECURSIVE
+// class Solution {
+// public:
+
+//     void preOrder(TreeNode* root , vector<int> &ans){
+//         if(root == NULL) return;
+//         ans.push_back(root->val);
+//         preOrder(root->left , ans);
+//         preOrder(root->right , ans);
+
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         preOrder(root , ans);
+//         return ans;         
+//     }
+// };
+
+// Method -2  Iterative 
 class Solution {
 public:
-
-    void preOrder(TreeNode* root , vector<int> &ans){
-        if(root == NULL) return;
-        ans.push_back(root->val);
-        preOrder(root->left , ans);
-        preOrder(root->right , ans);
-
-    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        preOrder(root , ans);
+        stack<TreeNode*> st;
+        if(root!=NULL)st.push(root);
+        while( st.size()>0 ){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left);
+        }
+        
         return ans;         
     }
 };
